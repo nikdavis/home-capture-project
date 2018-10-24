71251d074c58b4f304053e1e0adef3c4f3fe198c
Nikolas Davis <nikdavis@pdx.edu>
2018-10-24 01:45:07 -0700

    Try AWS upload

--- run.rb
+++ run.rb
@@ -3,7 +3,7 @@ require_relative './connection'
 require_relative './s3_runs'
 require_relative './security'
 
-bucket_name = 'home_capture'
+bucket_name = 'home_motion_capture'
 
 
 last_security_id = 0
@@ -12,11 +12,11 @@ if last_run
   last_security_id = last_run.last_security_id
 end
 
-puts Security.where('id > ?', last_security_id).length
-# get list of new files
-# query mysql, if null, start from >= 0
-# else grab last_security_id
-puts "hello"
-# s3 = Aws::S3::Resource.new(region:'us-west-2')
-# obj = s3.bucket('').object('key')
-# obj.upload_file('/path/to/source/file')
+new_images = Security.where('id > ?', last_security_id)
+
+image = new_images[0]
+name_wo_folder = image.filename.split('/')[1]
+
+s3 = Aws::S3::Resource.new(region:'us-west-2')
+obj = s3.bucket(bucket_name).object(name_wo_folder)
+obj.upload_file(image.filename)
