3094fc9e01960a06db3ebeefd36badd25fa4503d
Nikolas Davis <nikdavis@pdx.edu>
2018-10-24 01:21:28 -0700

    Start algorithm

--- run.rb
+++ run.rb
@@ -1,11 +1,18 @@
 require 'aws-sdk-s3'
-require_relative './schema'
+require_relative './connection'
 require_relative './s3_runs'
 require_relative './security'
 
 bucket_name = 'home_capture'
 
-puts Security.find(1)
+
+last_security_id = 0
+last_run = S3Runs.last
+if last_run
+  last_security_id = last_run.last_security_id
+end
+
+puts Security.where('id > ?', last_security_id).length
 # get list of new files
 # query mysql, if null, start from >= 0
 # else grab last_security_id
