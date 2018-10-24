9edeb4b09f696a86ee75452b27549337c46244d1
Nikolas Davis <nikdavis@pdx.edu>
2018-10-24 02:32:34 -0700

    Better names

--- run.rb
+++ run.rb
@@ -6,6 +6,8 @@ require_relative './security'
 
 bucket_name = 'home-motion-capture'
 
+day_format = '%Y-%m-%d'
+file_format = '#{day_format} '
 
 last_security_id = 0
 last_run = S3Runs.last
@@ -21,12 +23,15 @@ current_run = S3Runs.new()
 current_run.run_time = DateTime.now
 
 new_images.each_with_index do |image, i|
-  name_wo_folder = image.filename.split('/')[1]
+  ext = image.filename.split('.')[-1]
+  filename = image.time_stamp.strftime('#{day_format} %H.%M.%S') + '.' + ext
+  day_prefix = image.time_stamp.strftime(day_format)
 
+  key = day_prefix + '/' + filename
   s3 = Aws::S3::Resource.new(region:'us-west-2')
-  obj = s3.bucket(bucket_name).object(name_wo_folder)
-  obj.upload_file('../' + image.filename)
-  puts "Uploaded image #{i} of #{new_images.length}: #{name_wo_folder}"
+  obj = s3.bucket(bucket_name).object(key)
+  obj.upload_file(image.filename)
+  puts "Uploaded image #{i} of #{new_images.length}: #{filename}"
 end
 
 current_run.last_security_id = new_images.last.id
